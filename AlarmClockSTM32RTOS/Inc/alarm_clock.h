59c7ee50bfd25abf38380ad7aef5bee24350f71b
efryters <efryters@gmail.com>
2018-03-26 08:27:58 -0400

    Change clock settings in project

---
+++ AlarmClockSTM32RTOS/Inc/alarm_clock.h
@@ -0,0 +1,25 @@
+/*
+ * alarm_clock.h
+ *
+ *  Created on: Mar 25, 2018
+ *      Author: Eric
+ */
+
+#ifndef ALARM_CLOCK_H_
+#define ALARM_CLOCK_H_
+
+/*
+ * DEFINES
+ */
+
+
+/*
+ * Methods
+ */
+
+
+/*
+ * FreeRTOS tasks
+ */
+
+#endif /* ALARM_CLOCK_H_ */
