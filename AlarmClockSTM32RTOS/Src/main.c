634aae94932dedb2a5e83f07bd87d3f4e000860b
efryters <efryters@gmail.com>
2018-04-08 21:31:47 -0400

    Created branch for 7-segment Initial defines

--- AlarmClockSTM32RTOS/Src/main.c
+++ AlarmClockSTM32RTOS/Src/main.c
@@ -54,7 +54,7 @@
 #include "usb_host.h"
 
 /* USER CODE BEGIN Includes */
-
+#include "seg.h"
 /* USER CODE END Includes */
 
 /* Private variables ---------------------------------------------------------*/
@@ -356,7 +356,6 @@ static void MX_SPI1_Init(void)
         * EVENT_OUT
         * EXTI
      PC3   ------> I2S2_SD
-     PB10   ------> I2S2_CK
      PC7   ------> I2S3_MCK
 */
 static void MX_GPIO_Init(void)
@@ -366,28 +365,37 @@ static void MX_GPIO_Init(void)
 
   /* GPIO Ports Clock Enable */
   __HAL_RCC_GPIOE_CLK_ENABLE();
-  __HAL_RCC_GPIOC_CLK_ENABLE();
   __HAL_RCC_GPIOH_CLK_ENABLE();
+  __HAL_RCC_GPIOC_CLK_ENABLE();
   __HAL_RCC_GPIOA_CLK_ENABLE();
   __HAL_RCC_GPIOB_CLK_ENABLE();
   __HAL_RCC_GPIOD_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(CS_I2C_SPI_GPIO_Port, CS_I2C_SPI_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOE, CS_I2C_SPI_Pin|SEG_a_Pin|SEB_b_Pin|SEG_c_Pin 
+                          |SEG_d_Pin|SEG_e_Pin|SEG_f_Pin|SEG_g_Pin 
+                          |SEG_h_Pin|ANODE_A_Pin, GPIO_PIN_RESET);
 
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(OTG_FS_PowerSwitchOn_GPIO_Port, OTG_FS_PowerSwitchOn_Pin, GPIO_PIN_SET);
 
   /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(ANODE_B_GPIO_Port, ANODE_B_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(GPIOD, LD4_Pin|LD3_Pin|LD5_Pin|LD6_Pin 
                           |Audio_RST_Pin, GPIO_PIN_RESET);
 
-  /*Configure GPIO pin : CS_I2C_SPI_Pin */
-  GPIO_InitStruct.Pin = CS_I2C_SPI_Pin;
+  /*Configure GPIO pins : CS_I2C_SPI_Pin SEG_a_Pin SEB_b_Pin SEG_c_Pin 
+                           SEG_d_Pin SEG_e_Pin SEG_f_Pin SEG_g_Pin 
+                           SEG_h_Pin ANODE_A_Pin */
+  GPIO_InitStruct.Pin = CS_I2C_SPI_Pin|SEG_a_Pin|SEB_b_Pin|SEG_c_Pin 
+                          |SEG_d_Pin|SEG_e_Pin|SEG_f_Pin|SEG_g_Pin 
+                          |SEG_h_Pin|ANODE_A_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(CS_I2C_SPI_GPIO_Port, &GPIO_InitStruct);
+  HAL_GPIO_Init(GPIOE, &GPIO_InitStruct);
 
   /*Configure GPIO pin : OTG_FS_PowerSwitchOn_Pin */
   GPIO_InitStruct.Pin = OTG_FS_PowerSwitchOn_Pin;
@@ -416,13 +424,12 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(BOOT1_GPIO_Port, &GPIO_InitStruct);
 
-  /*Configure GPIO pin : CLK_IN_Pin */
-  GPIO_InitStruct.Pin = CLK_IN_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+  /*Configure GPIO pin : ANODE_B_Pin */
+  GPIO_InitStruct.Pin = ANODE_B_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  GPIO_InitStruct.Alternate = GPIO_AF5_SPI2;
-  HAL_GPIO_Init(CLK_IN_GPIO_Port, &GPIO_InitStruct);
+  HAL_GPIO_Init(ANODE_B_GPIO_Port, &GPIO_InitStruct);
 
   /*Configure GPIO pins : LD4_Pin LD3_Pin LD5_Pin LD6_Pin 
                            Audio_RST_Pin */
---
+++ AlarmClockSTM32RTOS/Src/seg.c
@@ -0,0 +1,11 @@
+/*
+ * seg.c
+ *
+ *  Created on: Apr 8, 2018
+ *      Author: eric
+ */
+
+#include "stm32f4xx_hal.h"
+#include "cmsis_os.h"
+#include "seg.h"
+
